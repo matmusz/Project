7b948f98d6b01ba1c21d837363e67101697652f6
mateusz <matemuszu@gmail.com>
2021-02-15 16:47:50 +0100

    initial setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+
+const indexRouter = require("./routes/index");
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    {useNewUrlParser: true},
+    () => (console.log("DB connected")))
+
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+app.use("/", indexRouter);
+
+app.listen(3000);
